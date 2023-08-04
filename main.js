b61b1fe1a12c1e946b950a57392aa2f4c93e683e
santos-vital <vital.leandro16@gmail.com>
2023-08-03 22:46:38 -0300

    Adicionando funcionalidades com javascript

---
+++ main.js
@@ -0,0 +1,43 @@
+// Variáveis da aplicação
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+let randomNumber = Math.round(Math.random() * 10);
+let xAttempts = 1;
+
+// Eventos
+btnTry.addEventListener('click', handleTryClick);
+btnReset.addEventListener('click', handleResetClick);
+document.addEventListener('keydown', function(e) {
+  if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+    handleResetClick();
+  }
+})
+
+// Função callback
+function handleTryClick(event) {
+  event.preventDefault(); // não faça o padrão
+
+  const inputNumber = document.querySelector("#inputNumber");
+
+  if(Number(inputNumber.value) == randomNumber) {
+    toggleScreen();
+
+    document.querySelector(".screen2 h2").innerText = `acertou em ${xAttempts} tentativas`;
+  }
+
+  inputNumber.value = "";
+  xAttempts++;
+}
+
+function handleResetClick() {
+  toggleScreen(); 
+  xAttempts = 1;
+  randomNumber = Math.round(Math.random() * 10);
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");  
+}
